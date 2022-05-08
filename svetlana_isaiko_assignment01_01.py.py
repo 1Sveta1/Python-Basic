efa150d386a2c90f89ebd73d49689e61b87ca82a
Sveta140 <svetlana.isayko1@gmail.com>
2022-05-08 23:04:29 +0300

    Second Version

--- svetlana_isaiko_assignment01_01.py.py
+++ svetlana_isaiko_assignment01_01.py.py
@@ -1,40 +1,18 @@
 #!python3
+import os
+
 
 # Assignment 01-01
 #
 # Assign actual data to the variables below
 # and run the script to print the output.
-def start_inp():
-    global name, profession, facility, city, country, goal
+def start_inp(d):
     name = input("name: ")
     profession = input("profession: ")
     facility = input("facility: ")
     city = input("city: ")
     country = input("country: ")
     goal = input("goal: ")
-    return name, profession, facility, city, country, goal
-
-
-def start_prnt():
-    global name, profession, facility, city, country, goal
-    name = "Sveta"
-    profession = "student"
-    facility = "college"
-    city = "Odesa"
-    country = "Ukraine"
-    goal = "python developer"
-    return name, profession, facility, city, country, goal
-
-
-name = " "
-profession = " "
-facility = " "
-city = " "
-country = " "
-goal = " "
-choose = int(input("1 - input data or 2 - print data: "))
-if choose == 1:
-    start_inp()
     d = {'name': name,
          'profession': profession,
          'facility': facility,
@@ -42,9 +20,24 @@ if choose == 1:
          'country': country,
          'goal': goal,
          }
+    return d
+
+
+def start_prnt(d):
     print("My name is " + d['name'] + " I am a " + d['profession'] + " at " + d['facility'] + "\nin " + d['city'] + ", "
           + d['country'] + "." + " I want to learn how to program \nin Python to become a " + d['goal'] + ".")
-if choose == 2:
-    start_prnt()
-    print("My name is " + name + " I am a " + profession + " at " + facility + "\nin " + city + ", "
-          + country + "." + " I want to learn how to program \nin Python to become a " + goal + ".")
+
+
+if __name__ == '__main__':
+    d = {'name': 'Sveta',
+         'profession': 'student',
+         'facility': 'college',
+         'city': 'Odesa',
+         'country': 'Ukraine',
+         'goal': 'Python developer',
+         }
+    choose = int(input("print data - 1, change - 0"))
+    if choose == 1:
+        start_prnt(d)
+    if choose == 0:
+        start_prnt(start_inp(d))
