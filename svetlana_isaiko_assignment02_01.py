91fb0315ef8a3ab74d0d6beb4df8d1d8e8454ee2
Sveta140 <svetlana.isayko1@gmail.com>
2022-05-09 23:09:43 +0300

    add task 2

---
+++ svetlana_isaiko_assignment02_01.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+# Assignment 02-01
+from math import pi
+
+
+def volume_calculation():
+    r, h = input_data()
+    volume = pi * r ** 2 * h
+    return volume
+
+
+def input_data():
+    r = float(input("input radius"))
+    h = float(input("input height"))
+    return r, h
+
+
+if __name__ == '__main__':
+    while True:
+        choose = int(input("1 - yes, 0 - no"))
+        if choose == 1:
+            volume = volume_calculation()
+            print(volume)
+        if choose == 0:
+            break
