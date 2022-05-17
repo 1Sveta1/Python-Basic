40f1109f2ca63f9ce3f57de8a1bf9c56486ad0ed
Sveta140 <svetlana.isayko1@gmail.com>
2022-05-17 21:56:57 +0300

    add task 4.1

---
+++ svetlana_isaiko_assignment04_01.py
@@ -0,0 +1,40 @@
+# Завдання: написати програму обчислення факторіалу числа.
+#           Программа запитує ввод від користувача у циклі
+#           (вихід можливий за допомогою Ctrl-C/D)
+# 
+# Факторіал числа: n!
+# n! = 1 * 2 * 3 * 4 ... n-1 * n
+# 0! = 1
+#
+# Приклад роботи:
+#
+# Введіть число для обчислення факторіалу: 5
+# 5! == 120
+# 
+# Введіть число для обчислення факторіалу: -5
+# Невірний ввод
+
+def input_data():
+    number = int(input("Введите число: "))
+    return number
+
+
+def calculation():
+    while True:
+        number = input_data()
+        factorial = 1
+        if number >= 0:
+            for i in range(2, number + 1):
+                factorial *= i
+            print(f"{number}! == {factorial}")
+        else:
+            print("Введите позитивное число")
+        choose = int(input("хотите узнать факториал числа? 1 - да: "))
+        if choose == 1:
+            continue
+        else:
+            break
+
+
+if __name__ == '__main__':
+    calculation()
